1aa2de13249edeeaf732852fa4fc66638336f1dc
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-11-16 04:33:35 -0500

    Added: axios default url

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import BASE_URL from './BASE_URL';
+
+axios.defaults.baseURL = BASE_URL;
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
